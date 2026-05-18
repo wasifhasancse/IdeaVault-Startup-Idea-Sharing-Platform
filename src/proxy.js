39031a8080d4a8783dd76ecfa87d5942783441c2
Wasif Hasan <wasifhasancse@gmail.com>
2026-05-18 23:29:22 +0600

    set proxy js

---
+++ src/proxy.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers(), // you need to pass the headers object.
+  });
+  if (!session) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/profile",
+    "/profile/:path",
+    "/add-idea",
+    "/my-ideas",
+    "/my-interactions",
+  ],
+};
